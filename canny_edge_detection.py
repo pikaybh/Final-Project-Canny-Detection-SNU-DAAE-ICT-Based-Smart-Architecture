d72d982fa3e7c558a5e6a317129c2afe1fcb1898
pikaybh <pikabh@naver.com>
2023-12-13 15:11:26 +0900

    blurring 삭제

--- canny_edge_detection.py
+++ canny_edge_detection.py
@@ -11,12 +11,11 @@ class CannyEdge:
         self.input_dir = _input_dir
         self.target = _target
         self.save_file_dir = _save_file_dir
-        self.image = cv2.imread(self.input_dir + '/' + self.target)
-        CannyEdge.image = blurring(self.image)
+        CannyEdge.image = cv2.imread(self.input_dir + '/' + self.target)
 
     def contouring(self, save = True) -> tuple:
         # 컨투어 찾기 전 이미지 전처리
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)# 이미지 출력
+        gray = cv2.cvtColor(CannyEdge.image, cv2.COLOR_BGR2GRAY)# 이미지 출력
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
         gray = cv2.bitwise_not(gray) # 객체보다 배경이 밝은 경우 이미지 반전
         # canny edge, threshold 등 다양한 전처리 시도 -> 객체와 배경을 가장 잘 분리하는 전처리 사용
@@ -30,7 +29,7 @@ if __name__ == "__main__":
     canny_obj = CannyEdge("D:/Projects/canny/scr/data/test", "smile_cat.jpg", "D:/Projects/canny/scr/data/test")
     image = canny_obj.image
     image2 = CannyEdge.image.copy()
-    gray, edge = canny_obj.contouring()
+    gray, edge = canny_obj.contouring(False)
 
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
     adaptive_threshold= cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
