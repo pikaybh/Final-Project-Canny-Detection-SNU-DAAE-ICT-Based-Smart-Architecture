0ed6518c088f0f6e7723de0058c47371ee5f24f4
pikaybh <pikabh@naver.com>
2023-12-13 20:44:00 +0900

    Data upload fail

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    input_dir = "<Change this>"
+    input_dir = "D:/Projects/canny/scr/data/building_crack_detection_image/Training/concrete/concrete_concrete_crack_source_09/converted2jpg"
     output_dir = "output"
     if not os.path.isdir(output_dir): os.mkdir
     for file in tqdm(os.listdir(input_dir)):
